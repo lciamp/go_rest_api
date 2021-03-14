cc759c27f7b9fd54796ac91d49a473965dea8948
lciamp <l.ciamp@yahoo.com>
2021-03-14 10:49:13 -0400

    add dummy DB using a  struct and a slice

--- main.go
+++ main.go
@@ -9,10 +9,29 @@ import (
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome Home!")
+	fmt.Fprintf(w, "Welcome Home!\n")
 }
 
 func main() {
+	// fake db using struck and slice
+	type event struct {
+		ID 			string `json:"ID"`
+		Title 		string `json:"Title"`
+		Description string `json:"Description"`
+
+	}
+
+	type allEvents []event
+
+	var events = allEvents {
+		{
+			ID: 		 "1",
+			Title: 		 "Intro to go.",
+			Description: "Intro to rest apis using go.",
+		},
+	}
+
+	// index page
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	log.Fatal(http.ListenAndServe(":8080", router))
