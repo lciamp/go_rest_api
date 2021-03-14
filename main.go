37c8a248902197338f78d84547eafb234f516946
lciamp <l.ciamp@yahoo.com>
2021-03-14 11:01:08 -0400

    add createEvent route

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -11,28 +13,43 @@ import (
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome Home!\n")
 }
+// fake db using struck and slice
+type event struct {
+	ID 			string `json:"ID"`
+	Title 		string `json:"Title"`
+	Description string `json:"Description"`
+
+}
+
+type allEvents []event
+
+var events = allEvents {
+	{
+		ID: 		 "1",
+		Title: 		 "Intro to go.",
+		Description: "Intro to rest apis using go.",
+	},
+}
+
+// function to add events
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Enter data with the event title and description only in order to update.")
+	}
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
+
+}
 
 func main() {
-	// fake db using struck and slice
-	type event struct {
-		ID 			string `json:"ID"`
-		Title 		string `json:"Title"`
-		Description string `json:"Description"`
-
-	}
-
-	type allEvents []event
-
-	var events = allEvents {
-		{
-			ID: 		 "1",
-			Title: 		 "Intro to go.",
-			Description: "Intro to rest apis using go.",
-		},
-	}
-
 	// index page
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/event", createEvent).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
